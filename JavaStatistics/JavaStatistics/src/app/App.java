f5e6919e48af6dc150d9243fa72a22760dc788a8
Kristopher Hamade <slash2223456@gmail.com>
2019-01-28 21:00:50 -0800

    Updates to code

--- JavaStatistics/JavaStatistics/src/app/App.java
+++ JavaStatistics/JavaStatistics/src/app/App.java
@@ -2,21 +2,19 @@ package app;
 
 import java.util.*;
 import java.util.Scanner;
-import java.lang.*;
 
 public class App {
+    String collectInput;
     public static void main(String[] args) throws Exception {
         
         Scanner input = new Scanner(System.in);
-        int count = 0;
 
-        double[] numbers = new double[count];
+        ArrayList<Double> numbers = new ArrayList<Double>();
 
-
+            //Perform tasks such as asking for user input. This also parses that input looking for ?, x, or X
             do{
-                
-                System.out.println("Enter Next Number or x X to exit");
-                String collectInput = input.nextLine();
+                System.out.println("Enter A Number to Add to Statistics. Enter ? to Print All Numbers Entered or Enter x or X to exit");
+                var collectInput = input.nextLine();
 
                 if (collectInput.equals("?")){
                     System.out.println("The user has input the following numbers" + numbers);
@@ -26,10 +24,18 @@ public class App {
                     System.out.println("Entered escape command. Exiting program...");
                     System.exit(0);
                 }else{
-                
+
+                //Adds the Collected User Data to the ArrayList
                 double dataCollected = Double.parseDouble(collectInput);
+                numbers.add(dataCollected);
+                //Print Information About the Array List
                 System.out.println("User Added Number: " + dataCollected);
-                System.out.println("Count has increased to" + count);
+                System.out.println("Count has increased to: " + numbers.size());
+                
+
+                System.out.println("The Average of All Numbers is: ");
+
+                System.out.println("Enter A Number to Add to Statistics. Enter ? to Print All Numbers Entered or Enter x or X to exit");
                 }
             }
             }while(input.hasNext());
