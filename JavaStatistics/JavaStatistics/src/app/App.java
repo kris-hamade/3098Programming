ff209ab06267e8357c94bef99b9289db4be268fe
Kristopher Hamade <slash2223456@gmail.com>
2019-01-28 20:07:08 -0800

    Updates

--- JavaStatistics/JavaStatistics/src/app/App.java
+++ JavaStatistics/JavaStatistics/src/app/App.java
@@ -15,21 +15,12 @@ public class App {
         double[] numbers = new double[count];
        
        
-        //Removed Code from the do while loop
-        //int i; i = count.length; i++
-        //input != x || input != X
-       /* System.out.println("Enter Something if it's an x or X we will quit hopefully");
-        double collectNumbers = input.nextDouble();
 
-        input.nextLine();
-        String collectText = input.nextLine();
-
-        if (collectText == "x" || collectText == "X") {
-            System.exit(1);
-        }*/
             do{
+                
                 System.out.println("Enter Next Number or x X to exit");
                 String collectInput = input.nextLine();
+
                 if (collectInput.equals("?")){
                     System.out.println("The user has input the following numbers" + numbers);
                 }else{
@@ -41,40 +32,10 @@ public class App {
                 
                 double dataCollected = Double.parseDouble(collectInput);
                 System.out.println("User Added Number: " + dataCollected);
+                System.out.println("Count has increased to" + count);
                 }
             }
             }while(input.hasNext());
-        
-
-
-        
-/*
-            do{
-                System.out.println("Enter Next Number or x X to exit");
-                collectInput = input.nextLine();
-                if (collectInput == "x" || collectInput == "X") {
-                    break;
-                }
-                double dataCollected = Double.parseDouble(collectInput);
-                System.out.println("User Added Number: " + dataCollected);
-
-            }while(input.hasNext()); */
-        /*
-        (input.hasNext() && input.equals("x") || input.equals("X")
-            while(true) {
-                System.out.println("Enter Next Number or x X to exit");
-                try {
-                    double collectNumbers = input.nextDouble();
-                    System.out.println("User Added Number: " + collectNumbers);
-                } catch (InputMismatchException e) {
-                    System.out.println("What a damn mess!");
-                } finally {
-                    System.exit(1);
-                }
-
-            }*/
-
-
-           
+    
     }
 }
