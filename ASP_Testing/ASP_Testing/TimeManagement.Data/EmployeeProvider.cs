4ee0cc438ec6657bb7581305101bd8a0c7fb775f
Kristopher Hamade <slash2223456@gmail.com>
2019-01-31 11:51:29 -0800

    New Project

---
+++ ASP_Testing/ASP_Testing/TimeManagement.Data/EmployeeProvider.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Data.SqlClient;
+using Dapper;
+
+namespace TimeManagement.Data
+{
+    public class EmployeeProvider : IEmployeeProvider
+    {
+        private readonly string connectionString;
+
+        public EmployeeProvider(string connectionString)
+        {
+            this.connectionString = connectionString;
+        } 
+
+        public IEnumerable<Employee> Get()
+        {
+            IEnumerable<Employee> employee = null;
+
+            using (var connection = new SqlConnection(connectionString))
+            {
+                employee = connection.Query<Employee>("select id, first_name as FirstName, last_name as LastName, address as Address, home_phone as HomePhone, cell_phone as CellPhone from Employee");
+
+            }
+
+
+            return employee;
+        }
+    }
+}
